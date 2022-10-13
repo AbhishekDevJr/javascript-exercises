285e3ddef195eed18f05c23aec23abb416d75e68
Abhishek Choudhari <abhishek007coc@gmail.com>
2022-10-13 19:43:38 +0530

    Exercise-03 Sol

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(testString) {
+    let outputString = '';
 
+    for(let i=0; i<testString.length; i++){
+
+        outputString = outputString + testString[(testString.length-1)-i];
+    }
+
+    return outputString;
 };
 
 // Do not edit below this line
