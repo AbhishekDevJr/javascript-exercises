56d199e0c5f6ece4c56a49ca205ad33071a52cdb
Abhishek Choudhari <abhishek007coc@gmail.com>
2022-10-13 18:24:39 +0530

    Exercise-2 Sol

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(testString, testNum) {
 
+    let outputString = '';
+    if(testNum < 0){
+        return 'ERROR';
+    }
+    else{    
+    for(let i=0; i<testNum; i++){
+        outputString = outputString + testString;
+    }
+    return outputString;
+}
 };
 
 // Do not edit below this line
