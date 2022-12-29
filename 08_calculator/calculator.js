c8b41fc1ed6b9833472be14a44bd8ac45557b70a
Abhishek Choudhari <abhishek007coc@gmail.com>
2022-12-30 01:03:13 +0530

    Calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,53 @@
-const add = function() {
-	
+const add = function(value1, value2) {
+    let tempSum = value1 + value2;
+    return tempSum;
 };
 
-const subtract = function() {
-	
+const subtract = function(value1, value2) {
+    let tempSub = value1 - value2;
+    return tempSub;
 };
 
-const sum = function() {
-	
+const sum = function(arrayValue) {
+    if(arrayValue.length === 0){
+        return 0;
+    }
+    else{
+        let sumArray = arrayValue.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue;
+        }, 0);
+        return sumArray;
+    }
 };
 
-const multiply = function() {
-
+const multiply = function(arrayValue) {
+    let mulArray = arrayValue.reduce((accumulator, currentValue) => {
+        let tempMulVal = accumulator * currentValue;
+        return tempMulVal;
+    }, 1);
+    return mulArray;
 };
 
-const power = function() {
-	
+const power = function(value1, value2) {
+    let tempVal = value1;
+    for(let i=0; i<value2-1; i++){
+        
+        tempVal = tempVal*value1;
+    }
+    return tempVal;
 };
 
-const factorial = function() {
+const factorial = function(facVal) {
+    let tempFac = 1;
+    let z = facVal;
+    if(facVal === 0 ){
+        return 1;
+    }else{
+        for(let i=facVal; i>1; i--){
+            tempFac = tempFac*i;
+        }
+        return tempFac;
+    }
 	
 };
 
