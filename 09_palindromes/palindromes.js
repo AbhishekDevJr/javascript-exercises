cea5f8aaf1d0c010d8be8672a0f54f3a673777d3
Abhishek Choudhari <abhishek007coc@gmail.com>
2022-12-30 16:42:42 +0530

    palindrome-done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,39 @@
-const palindromes = function () {
-
+const palindromes = function (stringParam) {
+    let realString = stringParam.replace(/\s/g,'');
+    realString = realString.replace(/!/g, '');
+    //realString = realString.replace(/./g, '');
+    realString = realString.replaceAll('.', '');
+    realString = realString.replaceAll(',', '');
+    realString = realString.toLowerCase();
+    console.log(realString);
+    let n = realString.length;
+    let checkVar = false;
+    if(realString.length % 2 === 0){
+        for(let i=0; i<realString.length; i++){
+            if(realString[i] === realString[n-i-1]){
+                checkVar = true;
+            }else{
+                checkVar = false;
+                break;
+            }
+        }
+        return checkVar;
+    }else{
+        for(let i=0; i<n; i++){
+            if(realString[i] === realString[n-i-1]){
+                checkVar = true;
+            }else{
+                checkVar = false;
+                break;
+            }
+        }
+        return checkVar;
+    }
 };
+
+// let checkCall = palindromes('hey .  , , T,!!h!ere');
+// console.log(checkCall);
+
 
 // Do not edit below this line
 module.exports = palindromes;
