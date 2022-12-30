14c53be1fa3286523b1cb5f62dbd5056756b37b0
Abhishek Choudhari <abhishek007coc@gmail.com>
2022-12-30 17:12:09 +0530

    fibonacci-completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthTerm) {
+    if(nthTerm < 0){
+        return 'OOPS';
+    }else{
+        nthTerm = parseInt(nthTerm);
+        let fibonacciArray = [1,1];
+        for(let i=2; i<100; i++){
+            fibonacciArray[i] = fibonacciArray[i-1] + fibonacciArray[i-2];
+        }
+        //console.table(fibonacciArray);
+        return (fibonacciArray[nthTerm-1]);
+}   
 };
-
+//console.log(fibonacci(3));
 // Do not edit below this line
 module.exports = fibonacci;
